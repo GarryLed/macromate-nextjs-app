8350587cf607bee711436c1c4d33d28df9b4092c
GarryLed <S00256356@atu.ie>
2025-06-05 16:43:53 +0100

    setting up jest for testing

---
+++ server/jest.config.ts
@@ -0,0 +1,12 @@
+
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    globals: {
+      'ts-jest': {
+        isolatedModules: true,
+      },
+    },
+  };
+  
